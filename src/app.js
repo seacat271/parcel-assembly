fc354e8cc7591aeec60953d9365ff30c6b755475
seacat271 <105187184+seacat271@users.noreply.github.com>
2023-06-08 13:02:08 +0300

    add Local

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+const refs = {
+  form: document.querySelector(".form"),
+  textarea: document.querySelector(".form textarea"),
+};
+
+refs.form.addEventListener("submit", onFormSubmit);
+refs.textarea.addEventListener("input", onTextareaInput);
+
+function onFormSubmit(event) {}
+function onTextareaInput(event) {
+  const message = event.currentTarget.value;
+  localStorage.setItem("message", message);
+}
