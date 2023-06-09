c5e1e5f46aa38402a6ca5aab85b9ff73f3cf5e73
seacat271 <105187184+seacat271@users.noreply.github.com>
2023-06-09 16:03:38 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,9 +5,21 @@ const refs = {
 
 refs.form.addEventListener("submit", onFormSubmit);
 refs.textarea.addEventListener("input", onTextareaInput);
+saveFeedback();
 
-function onFormSubmit(event) {}
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem("message");
+}
 function onTextareaInput(event) {
   const message = event.currentTarget.value;
   localStorage.setItem("message", message);
+}
+
+function saveFeedback() {
+  const savedMessage = localStorage.getItem("message");
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
 }
