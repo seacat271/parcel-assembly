8efb9bbc64cc0db43c3951623c29cba7be444f6d
seacat271 <105187184+seacat271@users.noreply.github.com>
2023-06-09 13:04:12 +0000

    Deploying to gh-pages from @ seacat271/parcel-assembly@c5e1e5f46aa38402a6ca5aab85b9ff73f3cf5e73 🚀

---
+++ index.f22fc302.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".form"),textarea:document.querySelector(".form textarea")};e.form.addEventListener("submit",function(e){e.preventDefault(),e.currentTarget.reset(),localStorage.removeItem("message")}),e.textarea.addEventListener("input",function(e){let t=e.currentTarget.value;localStorage.setItem("message",t)}),function(){let t=localStorage.getItem("message");t&&(e.textarea.value=t)}();
+//# sourceMappingURL=index.f22fc302.js.map
